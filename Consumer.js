b58b0fda9ce4c9b627978c70d39f447f299f9ec2
Tim Sanderson <twsand11@gmail.com>
2020-03-19 16:30:22 -0500

    Working solution

--- Consumer.js
+++ Consumer.js
@@ -18,9 +18,11 @@ amqp.connect('amqp://snpucpee:R1WnRCPBTgCiV01-p_L1c0_lebJScn21@mosquito.rmq.clou
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
         channel.consume(queue, (msg) => {
-          let msgContent = msg.content.toString();
-          console.log(" [x] Received %s", msgContent);
-          writeToFile(msgContent)
+          let brokerValue = msg.content.toString();
+          console.log(" [x] Received %s", brokerValue);
+          //get additional data from helper functions
+          let output = getOutput(Number(brokerValue));
+          openAndWrite(output)
             .then(() => {
               console.log(`Data written successfully!`);
               channel.ack(msg);
@@ -37,6 +39,42 @@ amqp.connect('amqp://snpucpee:R1WnRCPBTgCiV01-p_L1c0_lebJScn21@mosquito.rmq.clou
 });
 
 
+const calculatePV = () => {
+  let simulatedValue = Math.random() * 20;
+  return simulatedValue;
+}
+
+const getTotalOutputInKilowatts = (broker, pv) => {
+  let output = broker + pv;
+  output = output / 1000;
+  return Number(output.toFixed(2));
+}
+
+const getOutput = (broker) => {
+  let pv = calculatePV();
+  let totalOutputKW = getTotalOutputInKilowatts(broker, pv);
+  let output = `Broker: ${broker} watts, PV: ${pv} watts, Broker/PV in kW: ${totalOutputKW}kW, timestamp: ${Date.now()} \n`
+  return output;
+}
+
+const openAndWrite = (data) => {
+  return new Promise((resolve, reject) => {
+    fs.open('./Data.txt', 'wx', (err, fd) => {
+      if (err) {
+        appendToFile(data)
+          .then(() => resolve())
+          .catch((err) => reject(err))
+      } else {
+        console.log(fd);
+        writeToFile(data)
+          .then(() => resolve())
+          .catch((err) => reject(err))
+      }
+    })
+  })
+}
+
+
 const writeToFile = (data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile('./Data.txt', data, (err) => {
@@ -50,9 +88,9 @@ const writeToFile = (data) => {
   })
 };
 
-const appendToFile = () => {
+const appendToFile = (data) => {
   return new Promise((resolve, reject) => {
-    fs.appendFile("./Data.txt", "\nMOAR DATA", (err) => {
+    fs.appendFile("./Data.txt", data, (err) => {
       if (err) {
         console.log(err);
         reject(err);
